288d022d6b3ab86918c0afa0b8ddfedbc81fa151
Daniele Dalla Rosa <daniele.dalla.rosa77@gmail.com>
2020-11-16 17:43:07 +0100

    Added products to db, added product list page, product details not done

---
+++ static/script.js
@@ -0,0 +1,32 @@
+/**
+ * This function refresh the list of products
+ */
+function loadProducts() {
+
+    const ul = document.getElementById('products');
+
+    ul.innerHTML = '';
+
+    fetch('../api/v1/products')
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { // Here you get the data
+        
+        // console.log(data);
+        
+        return data.map(function(product) { // Map through the results and for each run the code below
+            
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+
+            span.innerHTML = `<a href="${product.self}"> Nome:${product.name} Descrizione:${product.description} Prezzo:${product.price} Quantità:${product.amount}</a>`;
+            
+            // Append all our elements
+            li.appendChild(span);
+            ul.appendChild(li);
+        })
+    })
+    .catch( error => console.error(error) );// If there is any error you will catch them here
+    
+}
+
+loadProducts();
