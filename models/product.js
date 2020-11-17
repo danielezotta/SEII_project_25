3d9dad0b6966f1f43769137fb6cf4b14ce05a419
Alex <alex.strappazzon@studenti.unitn.it>
2020-11-17 10:03:00 +0100

    implemented token controll (api/v1/buyproduct) and simple login to get it

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Product = mongoose.model('products', {
+
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    amount: { type: Number, required: true }
+});
+
+module.exports = Product;
