45008a1df0a81eb76d9bbbdf11c642172cf1276d
Beppe99993 <lamorgese.giuseppe99@gmail.com>
2020-11-17 14:11:50 +0100

    Implementation of 'adding product', 'product model' and authentication

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+const Product = mongoose.model('products', {
+    name: {type: String, required: true},
+    image: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    amount: {type: Number, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, required: true}
+});
+
+module.exports = Product;
