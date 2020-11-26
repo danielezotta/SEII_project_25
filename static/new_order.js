ab8fb01553eb0cb2df726fef652c2bbae06a5c22
Alex <alex.strappazzon@studenti.unitn.it>
2020-11-26 22:13:03 +0100

    rename buyproduct with order and fix orders.test.js with supertest

--- static/new_order.js
+++ static/new_order.js
@@ -4,15 +4,15 @@ VIS = 0;
  * This function is used to change the visibility of button to confirm the order of a product
  */
 function viewBtnForBuy() {
-    if (VIS == 0) {
+    if( VIS==0 ){
         document.getElementById("btnTAcquista").style.display = "none";
         document.getElementById("loadAcquista").style.display = "block";
         document.getElementById("btnAcquista").disable = true;
-    } else if (VIS == 1) {
+    }else if( VIS==1 ){
         document.getElementById("loadAcquista").style.display = "none";
         document.getElementById("btnTAcquista").style.display = "block";
         document.getElementById("btnAcquista").disable = false;
-    } else {
+    }else{
         var elem = document.getElementById('loadAcquista');
         elem.parentNode.removeChild(elem);
         var elem2 = document.getElementById('btnAcquista');
@@ -26,7 +26,7 @@ function viewBtnForBuy() {
 /*
  * Function that view an alert if a input is bad format
  */
-function alertInput(msg) {
+function alertInput(msg){
     var d = document.createElement("DIV");
     document.getElementById("alert_container").appendChild(d);
     d.innerHTML = `
@@ -41,9 +41,9 @@ function alertInput(msg) {
 
 //for simulate click
 function eventFire(el, etype) {
-    if (el.fireEvent) {
+    if(el.fireEvent){
         el.fireEvent('on' + etype);
-    } else {
+    }else{
         var evObj = document.createEvent('Events');
         evObj.initEvent(etype, true, false);
         el.dispatchEvent(evObj);
@@ -56,22 +56,22 @@ function eventFire(el, etype) {
 function viewStatusOrder(st, res) {
     var msg = "";
     var vis;
-    if (st == 400) {
-        if (res.error != null && res.error != "") {
+    if( st==400 ){
+        if( res.error!=null && res.error!="" ){
             msg = msg + "Problema formato dati inviati";
         }
         vis = 2;
-    } else if (st == 403) {
+    }else if( st==403 ){
         msg = "Permesso non consentito";
         document.getElementById("btnMLogin").style.display = "block";
         vis = 1;
-    } else if (st == 404) {
+    }else if( st==404 ){
         msg = "Prodotto non disponibile";
         vis = 2;
-    } else if (st == 500) {
+    }else if( st==500 ){
         msg = "Errore server, riprova più tardi";
         vis = 1;
-    } else {
+    }else{
         msg = "Prodotto comprato con successo";
         vis = 2;
     }
@@ -84,39 +84,39 @@ function viewStatusOrder(st, res) {
 /*
  * To control format of inputs
  */
-function ctrlInput() {
+function ctrlInput(){
 
     //control of "provincia"
     var pr = document.getElementById("inProvincia").value
-    if (pr == null || pr == "") {
+    if( pr==null || pr=="" ){
         alertInput("Provincia non valida");
         return false;
     }
 
     //control of "comune"
     var com = document.getElementById("inComune").value
-    if (com == null || com == "") {
+    if( com==null || com=="" ){
         alertInput("Comune non valido");
         return false;
     }
 
     //control of "via/localita'"
     var via = document.getElementById("inVia/localita").value
-    if (via == null || via == "") {
+    if( via==null || via=="" ){
         alertInput("Via/localit&#224 non valido");
         return false;
     }
 
     //control of "numero civico"
     var civ = document.getElementById("inCivico").value;
-    if( isNaN(civ) || civ<1 ) {
+    if( isNaN(civ) || civ<1 ){
         alertInput("Numero civico non valido");
         return false;
     }
 
     //control number of credit card
     var numCard = document.getElementById("inNumCard").value;
-    if (isNaN(numCard) || numCard<1 ) {
+    if( isNaN(numCard) || numCard<1 ){
         alertInput("Numero carta non valido");
         return false;
     }
@@ -125,16 +125,16 @@ function ctrlInput() {
     var cur_year = false;
     var expYCard = document.getElementById('inYExpCard').value
     var d = new Date();
-    if (isNaN(expYCard) || expYCard=="" || parseInt(expYCard)<d.getFullYear()) {
+    if( isNaN(expYCard) || expYCard=="" || parseInt(expYCard)<d.getFullYear() ){
         alertInput("Numero anno scadenza carta non valido");
         return false;
-    } else {
+    }else{
         cur_year = parseInt(expYCard) == d.getFullYear();
     }
 
     //control expiration month
     var expMCard = document.getElementById('inMExpCard').value;
-    if (isNaN(expMCard) || expMCard=="" || (expMCard < 1 || expMCard > 12) || (cur_year && parseInt(expMCard)<d.getMonth()) ) {
+    if( isNaN(expMCard) || expMCard=="" || (expMCard<1 || expMCard>12) || (cur_year && parseInt(expMCard)<d.getMonth()) ){
         alertInput("Numero mese scadenza carta non valido");
         return false;
     }
@@ -145,7 +145,7 @@ function ctrlInput() {
 /*
  * Fuction to send a request to confirm a order
  */
-function createOrder() {
+function createOrder(){
 
     viewBtnForBuy();
     VIS = 1;
@@ -161,7 +161,7 @@ function createOrder() {
     var token = localStorage.getItem('token');
 
     //control of input
-    if (!ctrlInput()) {
+    if( !ctrlInput() ){
         viewBtnForBuy();
         VIS = 0;
         return;
@@ -178,7 +178,7 @@ function createOrder() {
 
     //request to server
     var status;
-    fetch(`../api/v1/order/`, {
+    fetch(`../api/v1/orders/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
