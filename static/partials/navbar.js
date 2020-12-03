cdf4c1367934c9ef2b58aa982bd64c51745d781e
Daniele <danielezotta@gmail.com>
2020-12-03 18:22:05 +0100

    Addet REST API and some ui for cart

--- static/partials/navbar.js
+++ static/partials/navbar.js
@@ -1,21 +1,45 @@
 // HTML for the navbar, used in all pages to avoid repetition
-document.write(
-    `
+
+var navbar = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.html">SEII_project_25</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="collapse navbar-collapse" id="navbar">
             <div class="navbar-nav">
                 <a class="nav-link" href="index.html">Home</a>
             </div>
             <div class="navbar-nav ml-auto">
-                <a class="nav-link" href="cart.html">Carrello</a>
+    `;
+
+if (localStorage.getItem('token') !== null && localStorage.getItem('user_id') !== null) {
+    navbar += `
+                <a class="nav-link" href="cart.html">&#128722 Carrello <span id="navbar_cart_total" class="badge badge-light"></span></a>
+                <a class="nav-link" href="logout.html">Logout</a>
+            `;
+
+    fetch('../api/v1/cart/', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'x-access-token': localStorage.getItem('token'),
+            'user-id': localStorage.getItem('user_id')
+        }
+    })
+    .then(response => response.json())
+    .then(json => $("#navbar_cart_total").text(json.length));
+
+} else {
+    navbar += `
                 <a class="nav-link" href="login.html">Login</a>
-                <a class="nav-link" href="register.html">Registrati</a>
+            `;
+}
+
+navbar += `
             </div>
         </div>
     </nav>
-    `
-);
+    `;
+
+document.write(navbar);
