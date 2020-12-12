9d7de1341b13feab6e92367da691c4615af465d0
Beppe99993 <lamorgese.giuseppe99@gmail.com>
2020-12-13 00:02:54 +0100

    Added some comments

--- static/myOrders.js
+++ static/myOrders.js
@@ -1,4 +1,9 @@
+
+//Array of pair {product_id:nameProduct} of all products in the database
+//It serves to get the name of the product given the id
 var products = [];
+
+//It populates the array products
 
 function allProducts(){
   fetch('../api/v1/products',
@@ -7,7 +12,7 @@ function allProducts(){
             headers: {
                 'Content-Type': 'application/json',
             }})
-  .then((resp) => resp.json()) // Transform the data into json
+  .then((resp) => resp.json())
   .then(function(data) {
     data.map(function(product) {
           products[product._id] = product.name;
@@ -15,6 +20,8 @@ function allProducts(){
   })
   .catch( error => console.error(error) );
 }
+
+//It loads all the orders of the logged user
 
 function loadMyOrders(){
 
@@ -33,52 +40,54 @@ function loadMyOrders(){
                 'user-id': user_id,
                 'x-access-token': token
             }})
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) { // Here you get the data
+  .then((resp) => resp.json())
+  .then(function(data) {
 
-      var number_of_orders = data.length;
+    var number_of_orders = data.length;
 
-      var rows = [];
-      for (let i = 0; i < number_of_orders; i++) {
-          rows[i] = document.createElement('tr');
-      }
+    var rows = [];
+    for (let i = 0; i < number_of_orders; i++) {
+        rows[i] = document.createElement('tr');
+    }
 
-      var numRows = [];
-      var nameProducts = [];
-      var address = [];
-      var amount = [];
-      var timeStamp = [];
-      var numCard = [];
+    var numRows = [];
+    var nameProducts = [];
+    var address = [];
+    var amount = [];
+    var timeStamp = [];
+    var numCard = [];
 
-      data.map(function(product, i){
-          numRows[i] = document.createElement('th');
-          numRows[i].scope = "row";
-          numRows[i].innerHTML= i+1;
-          nameProducts[i] = document.createElement('td');
-          nameProducts[i].innerHTML = products[product.product_id];
-          address[i] = document.createElement('td');
-          address[i].innerHTML = product.address;
-          amount[i] = document.createElement('td');
-          amount[i].innerHTML = product.amount;
-          timeStamp[i] = document.createElement('td');
-          timeStamp[i].innerHTML = product.timeStamp;
-          numCard[i] = document.createElement('td');
-          numCard[i].innerHTML = product.numCard;
+    // Map through the results and for each run the code below
+    data.map(function(product, i){
+        numRows[i] = document.createElement('th');
+        numRows[i].scope = "row";
+        numRows[i].innerHTML= i+1;
+        nameProducts[i] = document.createElement('td');
+        nameProducts[i].innerHTML = products[product.product_id];
+        address[i] = document.createElement('td');
+        address[i].innerHTML = product.address;
+        amount[i] = document.createElement('td');
+        amount[i].innerHTML = product.amount;
+        timeStamp[i] = document.createElement('td');
+        timeStamp[i].innerHTML = product.timeStamp;
+        numCard[i] = document.createElement('td');
+        numCard[i].innerHTML = product.numCard;
 
-          rows[i].appendChild(numRows[i]);
-          rows[i].appendChild(nameProducts[i]);
-          rows[i].appendChild(address[i]);
-          rows[i].appendChild(amount[i]);
-          rows[i].appendChild(timeStamp[i]);
-          rows[i].appendChild(numCard[i]);
+        rows[i].appendChild(numRows[i]);
+        rows[i].appendChild(nameProducts[i]);
+        rows[i].appendChild(address[i]);
+        rows[i].appendChild(amount[i]);
+        rows[i].appendChild(timeStamp[i]);
+        rows[i].appendChild(numCard[i]);
 
-      });
+    });
 
-      rows.forEach(row => { // Add all rows to the container
-          bodyTable.appendChild(row);
-      });
+    // Add all rows to the container
+    rows.forEach(row => {
+        bodyTable.appendChild(row);
+    });
 
-      return;
+    return;
   })
   .catch( error => console.error(error) );
 }
